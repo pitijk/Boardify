c6c3bff5b7b5fe4fd12e0e735e04edcb3f098eab
Piotr Kielek <piotrek4kontak@gmail.com>
2019-10-23 14:07:49 +0200

    Updates

---
+++ precache-manifest.4f9ff69ac80d932c892c82e0329503c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6343a23229b7e1a8daa4cf5d4e2dd24e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ae9c44e9550a53b440ae",
+    "url": "/my-app/static/js/2.7151a08d.chunk.js"
+  },
+  {
+    "revision": "dccf2e1cfd5b61bda8e8",
+    "url": "/my-app/static/js/main.843b9196.chunk.js"
+  },
+  {
+    "revision": "3e0fcfccf7ed4beb4808",
+    "url": "/my-app/static/js/runtime-main.849f4150.js"
+  }
+]);
